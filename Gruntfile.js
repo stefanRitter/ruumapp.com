5411c7f4258a059d2b2d0cdda730abc5ee89905a
Stefan Ritter <stefan.ritter@sap.com>
2017-09-13 17:30:58 -0700

    add sales and make generic home

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,111 +2,126 @@
 
 module.exports = function (grunt) {
 
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-    stylus: {
-      compile: {
-        options: {
-          compress: true,
-          paths: ['src/**/*']
+        stylus: {
+            compile: {
+                options: {
+                    compress: true,
+                    paths: ['src/**/*']
+                },
+                files: {
+                    'dist/app.css': 'src/app.styl'
+                }
+            }
         },
-        files: {
-          'dist/app.css': 'src/app.styl'
-        }
-      }
-    },
 
-    jade: {
-      compile: {
-        options: {
-          client: false,
-          pretty: true
+        jade: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true
+                },
+                files: [ {
+                    cwd: 'src/',
+                    src: [
+                        'pages/**/*'
+                    ],
+                    dest: 'dist',
+                    expand: true,
+                    ext: '.html'
+                } ]
+            }
         },
-        files: [ {
-          cwd: 'src/',
-          src: [
-            'pages/**/*'
-          ],
-          dest: 'dist',
-          expand: true,
-          ext: '.html'
-        } ]
-      }
-    },
 
-    copy: {
-      main: {
-        files: [
-          {
-            expand: true,
-            flatten: true,
-            src: [
-              'src/CNAME',
-              'src/**/*.jpg',
-              'src/**/*.svg',
-              'src/**/*.png',
-              'src/**/*.mp4',
-              'src/**/*.ogv',
-              'src/**/*.css',
-              'src/*.ico',
-              'src/*.html',
-              'home/**/*',
-              ],
-            dest: 'dist/',
-            filter: 'isFile'}
-        ]
-      },
-      root: {
-        files: [
-          {
-            expand: true,
-            flatten: true,
-            src: [
-              'rootpages/**/*'
-              ],
-            dest: 'dist/',
-            filter: 'isFile'}
-        ]
-      },third: {
-      files: [
-        {
-          expand: true,
-          flatten: true,
-          src: [
-            'all/**/*'
-            ],
-          dest: 'dist/all/',
-          filter: 'isFile'}
-      ]
-    }
-    },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'src/CNAME',
+                            'src/**/*.jpg',
+                            'src/**/*.svg',
+                            'src/**/*.png',
+                            'src/**/*.mp4',
+                            'src/**/*.ogv',
+                            'src/**/*.css',
+                            'src/*.ico',
+                            'src/*.html',
+                            'home/**/*',
+                        ],
+                        dest: 'dist/',
+                        filter: 'isFile'}
+                    ]
+                },
+            root: {
+                    files: [
+                        {
+                            expand: true,
+                            flatten: true,
+                            src: [
+                                'rootpages/**/*'
+                            ],
+                            dest: 'dist/',
+                            filter: 'isFile'}
+                        ]
+            },
+            sales: {
+                        files: [
+                            {
+                                expand: true,
+                                flatten: true,
+                                src: [
+                                    'sales/**/*'
+                                ],
+                                dest: 'dist/sales/',
+                                filter: 'isFile'
+                            }
+                        ]
+                },
+            all: {
+                        files: [
+                            {
+                                expand: true,
+                                flatten: true,
+                                src: [
+                                    'all/**/*'
+                                ],
+                                dest: 'dist/all/',
+                                filter: 'isFile'
+                            }
+                        ]
+                }
+            },
 
-    watch: {
-      styles: {
-        files: ['src/**/*.styl'],
-        tasks: ['copy','stylus','jade']
-      },
-      html: {
-        files: ['oldhome/**/*','home/**/*', 'src/**/*.jade'],
-        tasks: ['copy','stylus','jade']
-      }
-    },
+                watch: {
+                    styles: {
+                        files: ['src/**/*.styl'],
+                        tasks: ['copy','stylus','jade']
+                    },
+                    html: {
+                        files: ['oldhome/**/*','home/**/*', 'src/**/*.jade'],
+                        tasks: ['copy','stylus','jade']
+                    }
+                },
 
-    'gh-pages': {
-      options: {
-        base: 'dist',
-        clone: 'node_modules/.tmp/'
-      },
-      src: ['**']
-    }
-  });
+                'gh-pages': {
+                    options: {
+                        base: 'dist',
+                        clone: 'node_modules/.tmp/'
+                    },
+                    src: ['**']
+                }
+            });
 
-  grunt.registerTask('build',   ['copy', 'stylus', 'jade']);
-  grunt.registerTask('publish', ['build', 'gh-pages']);
-  grunt.registerTask('default', ['build', 'watch']);
-};
+            grunt.registerTask('build',   ['copy', 'stylus', 'jade']);
+            grunt.registerTask('publish', ['build', 'gh-pages']);
+            grunt.registerTask('default', ['build', 'watch']);
+        };
